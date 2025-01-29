55d6afa72026eac77dda677d21b8273cb83e1985
Aeyah! <princessaeyah.palma@smc.pshs.edu.ph>
2025-01-29 20:59:58 +0800

    Create JAVAfile1.js
    The code for the first java file

---
+++ JAVAfile1.js
@@ -0,0 +1,7 @@
+let nickname = "Karen";
+let heightInInches = 62;
+let weightInKg = 50;
+
+let weightInLbs = weightInKg * 2.20462;
+
+alert(`Name: ${nickname}\nHeight: ${heightInInches}"\nWeight: ${weightInLbs.toFixed(2)} lbs`);
